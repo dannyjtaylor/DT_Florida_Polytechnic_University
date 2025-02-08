3db92a796cbd4d6872cc1900619acd496a14afb0
Daniel Taylor <daniel.taylor6456@gmail.com>
2025-02-08 18:04:09 -0500

    added some stuff

--- Computer_Engineering_Design_Lab_2/mathoperations.v
+++ Computer_Engineering_Design_Lab_2/mathoperations.v
@@ -1,3 +1,5 @@
+//don't pay attention to this, redone to do fulladders with 4-bit ripple carry adder implementation instead of just "A+B"
+
 module add(input [3:0] A, B, output [3:0] sum, output carryout);
 	assign sum = A + B;
 	assign carryout = sum > 4'b1111 ? 4'b0001 : 4'b0000;
