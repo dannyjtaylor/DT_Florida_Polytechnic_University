d683783859789a3e8f5301339cef2f308fdc20ed
Daniel Taylor <daniel.taylor6456@gmail.com>
2025-02-08 21:07:52 -0500

    fixed a mistake lollll

--- Computer_Architecture_and_Organization/Daniel_Taylor_Computer_Architecture_and_Organization_HWQ1.asm
+++ Computer_Architecture_and_Organization/Daniel_Taylor_Computer_Architecture_and_Organization_HWQ1.asm
@@ -1,11 +1,11 @@
 # program 2
 # Z = (A + B) X (C-D) A = 2, B = 4, C = 6, D = 3, Z = (2+4) * (6-3) = 6 * 3 = 18, Z should be 18. 
-#use PEMDAS, so parenthesis first, and then use mult from MIPS reference sheet to multiply
+#use PEMDAS, so parenthesis first, and then use mul from MIPS reference sheet to multiply
 
-addi $s1, $zero, 2  # A = 2
+addi $s1, $zero, 2   # A = 2
 addi $s2, $zero, 4   # B = 4
 addi $s4, $zero, 6   # C = 6
 addi $s3, $zero, 3,  # D = 3
 add $t0, $s1, $s2    # t0 = A + B 
-sub $t1, $s4, $t3    # t1 = C - D 
-mul $s5, $t0, $s1    # Z = (A+B) * (C-D)
+sub $t1, $s4, $s3    # t1 = C - D 
+mul $s5, $t0, $t1    # Z = (A+B) * (C-D)
